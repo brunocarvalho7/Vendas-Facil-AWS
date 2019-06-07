1f37db94bcc96a6192020a59bfdd5649f3f1ba15
Bruno Carvalho <brunocarvalho287@gmail.com>
2019-06-07 12:41:51 -0300

    Modificado o projeto para gerar WAR para o deploy no tomcat

--- back-end/vendas-facil/src/main/java/br/ufc/mobile/vendasfacil/VendasFacilApplication.java
+++ back-end/vendas-facil/src/main/java/br/ufc/mobile/vendasfacil/VendasFacilApplication.java
@@ -2,16 +2,24 @@ package br.ufc.mobile.vendasfacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @SpringBootApplication
-public class VendasFacilApplication {
+public class VendasFacilApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasFacilApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VendasFacilApplication.class);
+	}   
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
